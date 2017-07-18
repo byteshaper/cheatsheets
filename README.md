731d2340c958d675b72c8c90c9bc049917f2f896
Henning Schütz <henning@byteshaper.de>
2017-07-18 16:28:24 +0200

    gradle project structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cheatsheets
+Just a collection of code samples, mostly Java, to handle certain things.
+Most of them not runnable as they are...
+
+## Spring Boot Logging Config
+This example creates a logging config that logs to rolling files with maximum size of 50MB plus to the console when running spring-boot.
+It requires no additional dependencies except from spring-boot.
---
+++ src/main/java/com/byteshaper/cheatsheets/FileGzipper.java
@@ -0,0 +1,35 @@
+package com.byteshaper.cheatsheets;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.GZIPOutputStream;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Creates GZIP files.
+ *
+ */
+public class FileGzipper {
+
+    /**
+     * Creates a gzip file containing the given file, reusing it's original filename with adding a "*.gz"
+     * ending, in the same directory where the original file lives. The original file is not touched.
+     *
+     * @param inputFile
+     * @return path to the gzip file just generated
+     */
+    public static Path gzip(Path inputFile) throws IOException {
+        Path outputFile = Paths.get(inputFile.getParent().toString(), inputFile.getFileName() + ".gz");
+
+        try(GZIPOutputStream gzos = new GZIPOutputStream(new FileOutputStream(outputFile.toFile()));
+            FileInputStream fis = new FileInputStream(inputFile.toFile())) {
+
+            IOUtils.copy(fis, gzos);
+            return outputFile;
+
+        }
+    }
+}
