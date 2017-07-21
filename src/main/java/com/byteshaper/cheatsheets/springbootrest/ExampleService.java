246906c53a242da7a5a834d17c95454571e99b12
Henning Schuetz <henning.schuetz@zanox.com>
2017-07-21 16:59:38 +0200

    swagger conf example

--- src/main/java/com/byteshaper/cheatsheets/springbootrest/ExampleService.java
+++ src/main/java/com/byteshaper/cheatsheets/springbootrest/ExampleService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExampleService {
 
-    public String processInput(String input, int filterBy) {
+    public String processInput(String input, Integer filterBy) {
         return "I received input=" + input + ", filterBy=" + filterBy;
     }
 }
--- src/main/java/com/byteshaper/cheatsheets/springbootrest/RestExample.java
+++ src/main/java/com/byteshaper/cheatsheets/springbootrest/RestExample.java
@@ -1,5 +1,7 @@
 package com.byteshaper.cheatsheets.springbootrest;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,19 @@ public class RestExample {
     private ExampleService exampleService;
     
     @RequestMapping(value = "/example/{input}", method = RequestMethod.GET, produces = "application/json") 
+    @ApiOperation(
+            value = "This endpoint returns some magic stuff derived from the input :-)", 
+            consumes = "application/json",
+            produces = "text/plain")
     public Stuff getStuffAsJson(
-            @PathVariable("input") String input,
-            @RequestParam("filterBy") int filterBy) {
+            
+            @PathVariable("input") // required = true has no effect here
+            @ApiParam(value = "Input text", required = true) 
+            String input,
+            
+            @RequestParam(value = "filterBy", required = false) // => but has an effect here, and is picked up by Swagger as well
+            @ApiParam(value = "Some integer value") 
+            Integer filterBy) {
         Stuff stuff = new Stuff();
         stuff.message = exampleService.processInput(input, filterBy);
         stuff.number = 42;
