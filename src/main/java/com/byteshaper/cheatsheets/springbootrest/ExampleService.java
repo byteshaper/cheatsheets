795ce33b1f3bb662dbe508d6c343a279a1fbad8c
Henning Schuetz <henning.schuetz@zanox.com>
2017-11-12 17:53:46 +0100

    fixed logging

--- src/main/java/com/byteshaper/cheatsheets/springbootrest/ExampleService.java
+++ src/main/java/com/byteshaper/cheatsheets/springbootrest/ExampleService.java
@@ -1,16 +1,19 @@
 package com.byteshaper.cheatsheets.springbootrest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExampleService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleService.class);
+    
     public String processInput(String input, Integer filterBy) {
-    	/*
-        if(1 == 1) {
-            throw new NumberFormatException("Just a test");
-        }
-        */
+    	LOGGER.debug("Debug: {}", input);
+    	LOGGER.info("Info: {}", input);
+    	LOGGER.warn("Warn: {}", input);
+    	LOGGER.error("Error: {}", input);
         
         return "I received input=" + input + ", filterBy=" + filterBy;
     }
